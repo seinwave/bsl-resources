3f830117143b4cec5d9930b2275bfecc9f23fa34
Matt Seidholz <mseidholz@gmail.com>
2020-06-23 16:45:16 -0400

    almost done!

--- script.js
+++ script.js
@@ -8,38 +8,136 @@ function include(file) {
 } 
 include('./data/data.js')
 
+const search = document.getElementById("search-bar");
 
-// putting data into an array
-let dataList = [];
-dataList = [...data]
+let dataList = [...data]
 
 dataList.map(i => {
+  let fragment = document.createDocumentFragment();
+  let column = document.createElement('div')
+  column.className = "col-md-4"
+
+  let card = document.createElement('div')
+  card.className = "card mb-2"
+
+  let cardBody = document.createElement('div');
+  cardBody.className = "card-body";
+  cardBody.style = "transform: rotate(0)";
+
+  let cardTitle = document.createElement('h5');
+  cardTitle.className = "card-title mt-4";
+  cardTitle.textContent = i.title;
+
+  let cat1 = document.createElement('img');
+  cat1.src = `./assets/${i.category_1}.svg`;
+  cat1.className = "cat-icon"
+  cat1.alt = `${i.category_1}`;
+
+  let cat2 = document.createElement('img');
+  cat2.src = `./assets/${i.category_2}.svg`;
+  cat2.className = "cat-icon"
+  cat2.alt = `${i.category_2}`;
+
+  let cat3 = document.createElement('img');
+  cat3.src = `./assets/${i.category_3}.svg`;
+  cat3.className = "cat-icon"
+  cat3.alt = `${i.category_3}`;
+  
+  let cardHead = document.createElement('div');
+  cardHead.className = "row";
+  cardHead.appendChild(cat1);
+  cardHead.appendChild(cat2);
+  cardHead.appendChild(cat3);
+  
+  let cardSubHead = document.createElement('h6');
+  cardSubHead.className = "card-subtitle mb-5 text-muted"
+  cardSubHead.textContent = i.description;
+
+  cardBody.appendChild(cardHead);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardSubHead);
+
+  if (i.contact_name){
+    let contact = document.createElement('p');
+    contact.textContent = `Contact: ${i.contact_name}`
+    cardBody.appendChild(contact);}
+
+  if (i.contact_number){
+    let phone = document.createElement('p');
+    phone.textContent = `Call: ${i.contact_number}`
+    cardBody.appendChild(phone);}
+
+  if (i.contact_email){
+    let email = document.createElement('p');
+    email.textContent = `Email: ${i.contact_email}`
+    cardBody.appendChild(email);}
+
+  if (i.url){
+  let cardButton = document.createElement('a');
+  cardButton.href = i.url;
+  cardButton.className = "btn btn-primary mb-3";
+  cardButton.textContent = "Visit resource"
+  cardButton.target = "_blank";
+  cardBody.appendChild(cardButton);}
+
+  card.appendChild(cardBody);
+
+  fragment.appendChild(card)
+  column.appendChild(fragment);
+
+  let resourceList = document.getElementById('resource-list');
+  resourceList.appendChild(column)
+})
+
+let searchText = ""
+
+search.addEventListener('keyup', function () {
+  let resourceList = document.getElementById('resource-list');
+  resourceList.innerHTML = "";
+  searchText = search.value.toString();
+  console.log(searchText);
+  return filterMap(searchText);
+});
+
+filterMap = (searchText) => {
+  console.log("FilterMap", searchText)
+  return dataList.filter(i => { return i.title.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.service_area.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.category_1.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.category_2.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.category_3.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.contact_name.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.contact_email.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.contact_number.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.address.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.url.toLowerCase().includes(searchText.toLowerCase()) ||
+    i.description.toLowerCase().includes(searchText.toLowerCase())
+  }).map(i => {
     let fragment = document.createDocumentFragment();
     let column = document.createElement('div')
     column.className = "col-md-4"
-
-    
+  
     let card = document.createElement('div')
     card.className = "card mb-2"
-
+  
     let cardBody = document.createElement('div');
     cardBody.className = "card-body";
     cardBody.style = "transform: rotate(0)";
-
+  
     let cardTitle = document.createElement('h5');
     cardTitle.className = "card-title mt-4";
     cardTitle.textContent = i.title;
-
+  
     let cat1 = document.createElement('img');
     cat1.src = `./assets/${i.category_1}.svg`;
     cat1.className = "cat-icon"
     cat1.alt = `${i.category_1}`;
-
+  
     let cat2 = document.createElement('img');
     cat2.src = `./assets/${i.category_2}.svg`;
     cat2.className = "cat-icon"
     cat2.alt = `${i.category_2}`;
-
+  
     let cat3 = document.createElement('img');
     cat3.src = `./assets/${i.category_3}.svg`;
     cat3.className = "cat-icon"
@@ -51,33 +149,29 @@ dataList.map(i => {
     cardHead.appendChild(cat2);
     cardHead.appendChild(cat3);
     
-
-
-    
-
     let cardSubHead = document.createElement('h6');
     cardSubHead.className = "card-subtitle mb-5 text-muted"
     cardSubHead.textContent = i.description;
-
+  
     cardBody.appendChild(cardHead);
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(cardSubHead);
-
+  
     if (i.contact_name){
       let contact = document.createElement('p');
       contact.textContent = `Contact: ${i.contact_name}`
       cardBody.appendChild(contact);}
-
+  
     if (i.contact_number){
       let phone = document.createElement('p');
       phone.textContent = `Call: ${i.contact_number}`
       cardBody.appendChild(phone);}
-
+  
     if (i.contact_email){
       let email = document.createElement('p');
       email.textContent = `Email: ${i.contact_email}`
       cardBody.appendChild(email);}
-
+  
     if (i.url){
     let cardButton = document.createElement('a');
     cardButton.href = i.url;
@@ -85,17 +179,125 @@ dataList.map(i => {
     cardButton.textContent = "Visit resource"
     cardButton.target = "_blank";
     cardBody.appendChild(cardButton);}
-
-    
-
-    
-    
-
+  
     card.appendChild(cardBody);
-
+  
     fragment.appendChild(card)
     column.appendChild(fragment);
+  
+    let resourceList = document.getElementById('resource-list');
+    resourceList.appendChild(column)
+  })
+  
+}
 
-    let borp = document.getElementById('borp');
-    borp.appendChild(column)
-})
+// dataList.filter( i => {
+//   return i.title.toLowerCase().includes(searchText.toLowerCase()) 
+//   i.service_area.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.category_1.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.category_2.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.category_3.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.contact_name.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.contact_email.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.contact_number.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.address.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.url.toLowerCase().includes(searchText.toLowerCase()) ||
+//   i.description.toLowerCase().includes(searchText.toLowerCase())
+// })
+
+
+  
+// dataList.map(i => {
+//   let fragment = document.createDocumentFragment();
+//   let column = document.createElement('div')
+//   column.className = "col-md-4"
+
+  
+//   let card = document.createElement('div')
+//   card.className = "card mb-2"
+
+//   let cardBody = document.createElement('div');
+//   cardBody.className = "card-body";
+//   cardBody.style = "transform: rotate(0)";
+
+//   let cardTitle = document.createElement('h5');
+//   cardTitle.className = "card-title mt-4";
+//   cardTitle.textContent = i.title;
+
+//   let cat1 = document.createElement('img');
+//   cat1.src = `./assets/${i.category_1}.svg`;
+//   cat1.className = "cat-icon"
+//   cat1.alt = `${i.category_1}`;
+
+//   let cat2 = document.createElement('img');
+//   cat2.src = `./assets/${i.category_2}.svg`;
+//   cat2.className = "cat-icon"
+//   cat2.alt = `${i.category_2}`;
+
+//   let cat3 = document.createElement('img');
+//   cat3.src = `./assets/${i.category_3}.svg`;
+//   cat3.className = "cat-icon"
+//   cat3.alt = `${i.category_3}`;
+  
+//   let cardHead = document.createElement('div');
+//   cardHead.className = "row";
+//   cardHead.appendChild(cat1);
+//   cardHead.appendChild(cat2);
+//   cardHead.appendChild(cat3);
+  
+
+
+  
+
+//   let cardSubHead = document.createElement('h6');
+//   cardSubHead.className = "card-subtitle mb-5 text-muted"
+//   cardSubHead.textContent = i.description;
+
+//   cardBody.appendChild(cardHead);
+//   cardBody.appendChild(cardTitle);
+//   cardBody.appendChild(cardSubHead);
+
+//   if (i.contact_name){
+//     let contact = document.createElement('p');
+//     contact.textContent = `Contact: ${i.contact_name}`
+//     cardBody.appendChild(contact);}
+
+//   if (i.contact_number){
+//     let phone = document.createElement('p');
+//     phone.textContent = `Call: ${i.contact_number}`
+//     cardBody.appendChild(phone);}
+
+//   if (i.contact_email){
+//     let email = document.createElement('p');
+//     email.textContent = `Email: ${i.contact_email}`
+//     cardBody.appendChild(email);}
+
+//   if (i.url){
+//   let cardButton = document.createElement('a');
+//   cardButton.href = i.url;
+//   cardButton.className = "btn btn-primary mb-3";
+//   cardButton.textContent = "Visit resource"
+//   cardButton.target = "_blank";
+//   cardBody.appendChild(cardButton);}
+
+  
+
+  
+  
+
+//   card.appendChild(cardBody);
+
+//   fragment.appendChild(card)
+//   column.appendChild(fragment);
+
+//   let resourceList = document.getElementById('resource-list');
+//   resourceList.appendChild(column)
+// })
+
+
+
+
+// putting data into an array
+
+
+
